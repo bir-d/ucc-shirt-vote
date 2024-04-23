ac336e938b519b28403bb5c2364c63115c5e2694
bird <bird@ucc.asn.au>
2024-04-23 21:45:21 +0800

    more auth module stuff, can move on to hooking it up with DB soon

--- cgi-bin/auth.py
+++ cgi-bin/auth.py
@@ -1,14 +1,20 @@
+import os
 import subprocess
 import json
 import base64
 import cgi
 from cryptography.fernet import Fernet
+from enum import Enum
+from http import cookies
 from typing import Optional
 
 ENCRYPTION_TOKEN = "changeMe"
 fernet = Fernet(ENCRYPTION_TOKEN)
 form = cgi.FieldStorage()
 
+class Commands(Enum):
+    GET_TOKEN = 0
+    VOTE = 1
 
 class Credentials:
     username: str
@@ -52,17 +58,53 @@ def decode_token(token: str) -> Credentials:
 
     return Credentials(cred_dict.get("username"), cred_dict.get("password"))
 
-
+# Valid inputs:
+# Token + command
+# Credentials + command
 # Returns an error message or `None`
-def verify_input(form: cgi.FieldStorage) -> Optional[str]:
-    token = form.getvalue("token")
+def verify_input(form: cgi.FieldStorage, cookies: cookies.SimpleCookie) -> Optional[str]:
+    token = cookies["token"].value
     username = form.getvalue("username")
     password = form.getvalue("password")
     command = form.getvalue("command")
 
-    # check input here...
+    if (command is None):
+        return "No command."
+    
+    if token is not "":
+        if (username is not None or password is not None):
+            return "Token and credentials given."
+        return None
+        
+    if not (username is not None and password is not None):
+        return "Missing username or password."
 
     return None
 
+def load_cookies() -> cookies.SimpleCookie:
+    cookies_instance: cookies.SimpleCookie = cookies.SimpleCookie()
+    cookies_string: Optional[str] = os.environ.get('HTTP_COOKIE')
+    if cookies_string is not None:
+        cookies_instance.load(cookies_string)
+    return cookies_instance
 
-verify_input(form)
+input_cookies = load_cookies()
+verify_input(form, input_cookies)
+
+command = form.getvalue("command")
+match command:
+    case Commands.GET_TOKEN:
+        credentials = Credentials(form.getvalue("username"), form.getvalue("password"))
+        if (check_credentials(credentials)):
+            c: cookies.SimpleCookie = cookies.SimpleCookie()
+            c.load(encode_token(credentials))
+            # set token
+            exit()
+    case Commands.VOTE:
+        token = input_cookies["token"].value
+        credentials = decode_token(token)
+        if (check_credentials(credentials)):
+            # vote for user in credentials, exit
+            exit()
+    case _:
+        exit()
