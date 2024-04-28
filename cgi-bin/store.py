cb8694ab92c8a5beb603bd114074f69e65e5e98d
bird <20701908+bir-d@users.noreply.github.com>
2024-04-28 17:32:06 +0800

    use alpine store so we can pass to backend

---
+++ cgi-bin/store.py
@@ -0,0 +1,26 @@
+import sqlite3
+import get_shirts
+
+
+def get_id_object(ids: list) -> str:
+    obj = "Alpine.store('shirt_votes', {"
+    for id in ids:
+        obj += f"{id}: 0,"
+    obj += "})"
+    return obj
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect("db.sqlite")
+    shirts = get_shirts.get_shirts(conn.cursor())
+    id_object = get_id_object([shirt.id for shirt in shirts])
+
+    print("Content-Type: text/html")
+    print()
+    print(
+        f"""
+        document.addEventListener('alpine:init', () => {{
+            {id_object}
+        }})
+    """
+    )
